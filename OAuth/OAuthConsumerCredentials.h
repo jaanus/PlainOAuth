a8e0ea41d8a25c92551612ff444378e2c8ce9679
Jaanus <jaanus@gmail.com>
2010-06-24 22:48:36 -0700

    sample consumer credentials

---
+++ OAuth/OAuthConsumerCredentials.h
@@ -0,0 +1,12 @@
+/*
+ *  OAuthConsumerCredentials.h
+ *
+ *  Created by Jaanus Kase on 16.01.10.
+ *  Copyright 2010. All rights reserved.
+ *
+ */
+
+#error Replace these with your own app credentials, then remove this error.
+
+#define OAUTH_CONSUMER_KEY @"insert your consumer key here"
+#define OAUTH_CONSUMER_SECRET @"insert your consumer secret here"
